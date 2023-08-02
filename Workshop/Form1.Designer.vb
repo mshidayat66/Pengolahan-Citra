ea8b37daf595b6433ac18aa9eca557f738312170
Muhammad Syarif Hidayat <92723671+mshidayat66@users.noreply.github.com>
2023-08-02 22:48:36 +0700

    Add Project

---
+++ Workshop/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        OpenFileDialog1 = New OpenFileDialog()
+        PictureBox1 = New PictureBox()
+        PictureBox2 = New PictureBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' OpenFileDialog1
+        ' 
+        OpenFileDialog1.FileName = "OpenFileDialog1"
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(-2, -1)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(152, 134)
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), Image)
+        PictureBox2.Location = New Point(180, -1)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(152, 134)
+        PictureBox2.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox2.TabIndex = 4
+        PictureBox2.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(775, 450)
+        Controls.Add(PictureBox2)
+        Controls.Add(PictureBox1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+End Class
---
+++ Workshop/Form1.vb
@@ -0,0 +1,49 @@
+﻿Imports System.Drawing.Imaging
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim originalImage As Bitmap = DirectCast(PictureBox1.Image, Bitmap)
+        Dim colorSplitImage As Bitmap = SplitColorVerticallyWithBrightness(originalImage, 50)
+
+        PictureBox2.Image = colorSplitImage
+    End Sub
+
+    Private Function SplitColorVerticallyWithBrightness(image As Bitmap, brightnessValue As Integer) As Bitmap
+        Dim width As Integer = image.Width
+        Dim height As Integer = image.Height
+        Dim resultImage As New Bitmap(width, height)
+
+        For x As Integer = 0 To width - 1
+            For y As Integer = 0 To height - 1
+                Dim pixel As Color = image.GetPixel(x, y)
+                Dim red As Integer = 0
+                Dim green As Integer = 0
+                Dim blue As Integer = 0
+                Dim position As Integer = x * 5 / width
+
+                Select Case position
+                    Case 0
+                        red = Math.Min(255, pixel.R + brightnessValue)
+                    Case 1
+                        green = Math.Min(255, pixel.G + brightnessValue)
+                    Case 2
+                        blue = Math.Min(255, pixel.B + brightnessValue)
+                    Case 3
+                        red = Math.Min(255, pixel.R + brightnessValue)
+                        green = Math.Min(255, pixel.G + brightnessValue)
+                    Case 4
+                        Dim gray As Integer = CInt(pixel.R * 0.3 + pixel.G * 0.59 + pixel.B * 0.11)
+                        red = gray
+                        green = gray
+                        blue = gray
+                End Select
+
+                Dim newPixel As Color = Color.FromArgb(red, green, blue)
+
+                resultImage.SetPixel(x, y, newPixel)
+            Next
+        Next
+
+        Return resultImage
+    End Function
+End Class
